c1ee04542b772dfbf3eda51f40a1822bece3ae49
Paul Magister <pamagister@protonmail.com>
2024-06-23 22:20:23 +0200

    #4 use unittest

--- src/gpxpert/WaypointTableConverter/test_WaypointTableConverter.py
+++ src/gpxpert/WaypointTableConverter/test_WaypointTableConverter.py
@@ -1,9 +1,24 @@
 from gpxpert.WaypointTableConverter.WaypointTableConverter import WaypointTableConverter
 
+import unittest
 
-def test_ConvertToGpx():
-    tableFile = '../../../res/test/waypoints_table.txt'
-    converter = WaypointTableConverter(tableFile)
-    success = converter.ConvertToGpx()
-    assert success
 
+class WaypointTableConverterTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        tableFile = '../../../res/test/waypoints_table.txt'
+        converter = WaypointTableConverter(tableFile)
+        cls.gpxFileName: str = converter.ConvertToGpx()
+
+
+    def test_ConvertToGpx(self):
+        assert self.gpxFileName
+        assert self.gpxFileName.endswith('gpx')
+
+    def test_ConvertToGpx_VerifyContent(self):
+        expectedWaypointString = \
+            '  <wpt lat="46.006348" lon="8.970043">\n' \
+            '    <name>9 Monte Bre - Monte Boglia Cassarate</name>\n'\
+            '  </wpt>'
+        with open(self.gpxFileName, 'r') as gpxFile:
+            assert expectedWaypointString in gpxFile.read()
